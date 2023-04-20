bf1b07582273d123c6fdbbd0ec79b277aa6f5aa9
unknown <lilskyex0x.apk@gmail.com>
2023-04-20 16:05:44 +0630

    Add comment popup function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import "./style.css";
+import { openModal, closeModal } from "./modules/popup";
+import { modal } from "./modules/popup";
+
+const cmdButtonOpen = document.querySelectorAll(".button");
+const cmdButtonClose = document.querySelectorAll(".close-btn");
+
+cmdButtonOpen.forEach((button) => {
+  button.addEventListener("click", () => {
+    openModal(modal);
+  });
+});
+
+cmdButtonClose.forEach((button) => {
+  button.addEventListener("click", () => {
+    closeModal(modal);
+  });
+});
