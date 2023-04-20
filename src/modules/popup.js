6684895812313dbd7298b99104b41862e1547403
unknown <lilskyex0x.apk@gmail.com>
2023-04-20 20:03:31 +0630

    Replace the popup section dynamically with the module

--- src/modules/popup.js
+++ src/modules/popup.js
@@ -1,3 +1,35 @@
+const popupSection = document.createElement("section");
+popupSection.setAttribute("id", "popupModal");
+popupSection.classList.add("modal");
+popupSection.innerHTML = `
+<div class="popup-wrapper">
+    <button class="close-btn">&times;</button>
+    <img src="./assets/artlogo.png" alt="">
+    <h2>Artwork</h2>
+    <div class="art-details">
+        <ul>
+            <li>Fuel: titanium</li>
+            <li>Weight: 400</li>
+            <li>Length: 100,000</li>
+            <li>Power: 100,000,000</li> 
+        </ul>
+    </div>
+    <div class="cmds">
+        <h3>Comments (3)</h3>
+        <p>Skye: I love it</p>
+        <p>Meave: I would buy that one, its AWESOME</p>
+    </div>
+    <div class="cmd-form">
+        <form action="#" class="form">
+            <input type="text" name="name" id="name" placeholder="Your Name">
+            <input type="text" name="insight" id="insights" placeholder="Your insights">
+            <input type="button" value="Comment" id="cmd-btn">
+        </form>
+    </div>
+</div>
+<div id="overlay"></div>`;
+document.body.appendChild(popupSection);
+
 export const modal = document.getElementById("popupModal");
 const overlay = document.getElementById("overlay");
 
