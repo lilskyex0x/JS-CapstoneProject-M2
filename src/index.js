b1ed404d1c42d79257d53bd833c896bec0f7a0df
unknown <lilskyex0x.apk@gmail.com>
2023-04-20 22:03:09 +0630

    Delete extra functions

--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
 import "./style.css";
 import Api from "./modules/api.js";
-import { openModal, closeModal } from "./modules/popup";
-import { modal } from "./modules/popup";
 
 const callAPI = new Api();
 
 callAPI.GetArtworks();
-
-const cmdButtonOpen = document.querySelectorAll(".button");
-const cmdButtonClose = document.querySelectorAll(".close-btn");
-
-cmdButtonOpen.forEach((button) => {
-  button.addEventListener("click", () => {
-    openModal(modal);
-  });
-});
-
-cmdButtonClose.forEach((button) => {
-  button.addEventListener("click", () => {
-    closeModal(modal);
-  });
-});
-
