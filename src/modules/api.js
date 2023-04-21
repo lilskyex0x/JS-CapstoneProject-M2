ab0cbfe763c99ba9fa1bba51807356115d10aecb
mercymugambi <mercymugambi2016@gmail.com>
2023-04-21 11:50:57 +0300

    Add likes function

--- src/modules/api.js
+++ src/modules/api.js
@@ -2,6 +2,8 @@ import DisplayCards from "./listCards.js";
 
 export default class Api {
   constructor() {
+    this.InvolvementApiEP = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
+    this.this.InvolvementAppID = 'nt6MV6FCiUNpFq4MPkDA';
     this.artworksList = 'https://api.artic.edu/api/v1/artworks?page=2';
   }
 
--- src/modules/listCards.js
+++ src/modules/listCards.js
@@ -4,6 +4,7 @@ import { modal } from "./popup.js";
 const Cards = document.querySelector(".cards");
 
 async function DisplayCards(data) {
+  const numOfLikes = data;
   data.forEach((element, index) => {
     Cards.innerHTML += `
       <div class="card">
@@ -15,15 +16,22 @@ async function DisplayCards(data) {
           </div>
         </div>
         <div class="title-container">
-          <h3>${element.title} <i class="fa fa-heart"></i></h3>
-          <div class="interactions">
-            <div class="likes">355 likes</div>
+          <h3>${element.title} <i class="fa fa-heart like-button"></i></h3>
+          <div class="interactions show-likes">
+            <div class="${numOfLikes[index].likes} likes</div>
           </div>
           <button class="button comment"><i class="fa fa-comments"></i> Comments</button>
         </div>
       </div>
     `;
   });
+
+  // ADD EVENTS TO LIKE BUTTON
+  const likeButtons = document.querySelectorAll('.like-button');
+  likeButtons.forEach((likeButton, index) => likeButton.addEventListener('click', () => {
+    postLikes(index);
+    getLikes(data);
+  }));
 
   const commentButtons = document.querySelectorAll(".comment");
   commentButtons.forEach((button, index) => {
