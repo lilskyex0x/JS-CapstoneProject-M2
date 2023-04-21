834e6b9785a664a437a4eea3ad0c074f4d87fa12
mercymugambi <mercymugambi2016@gmail.com>
2023-04-21 22:48:42 +0300

    Add product count

--- src/modules/api.js
+++ src/modules/api.js
@@ -2,51 +2,24 @@ import DisplayCards from "./listCards.js";
 
 export default class Api {
   constructor() {
-    this.artworksList = "https://api.artic.edu/api/v1/artworks?page=2";
-    this.baseUrl =
-      "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps";
-    this.appId = "rZVQvLjlhB3dnDtkMDhH";
+    this.artworksList = 'https://api.artic.edu/api/v1/artworks?';
   }
 
-  async fetchLikes() {
-    try {
-      const response = await fetch(`${this.baseUrl}/${this.appId}/likes`);
-      if (response.ok) {
-        const data = response.json();
-        return data;
-      }
-      return [];
-    } catch (err) {
-      return err;
+  GetArtworks = async () => {
+    let artworks = [];
+    const cachedData = localStorage.getItem('artworks');
+    if (cachedData) {
+      artworks = JSON.parse(cachedData);
+      DisplayCards(artworks);
+      return;
     }
-  }
 
-  async GetArtworks() {
-    const response = await fetch(`${this.artworksList}`);
-    const json = await response.json();
-    const artworks = json.data;
-
-    const promises = artworks.map(async (artwork, index) => {
-      const likes = await this.fetchLikes(artwork.id);
-      return {
-        id: artwork.id,
-        title: artwork.title,
-        likes: likes,
-        imgIndex: index,
-      };
-    });
-
-    const data = await Promise.all(promises);
-    DisplayCards(data);
-  }
-
-  async addLikes(likes) {
-    await fetch(`${this.baseUrl}/${this.appId}/likes`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(likes),
-    });
+    await fetch(`${this.artworksList}`)
+      .then((response) => response.json())
+      .then((json) => {
+        artworks = json.data;
+        localStorage.setItem('artworks', JSON.stringify(artworks));
+        DisplayCards(artworks);
+      });
   }
 }
--- src/modules/listCards.js
+++ src/modules/listCards.js
@@ -1,20 +1,9 @@
-import Api from "./api.js";
 import { openModal, modal } from "./popup.js";
 
 async function DisplayCards(data) {
   const Cards = document.querySelector(".cards");
-  const likes = {};
 
-  // Fetch likes for each artwork
-  for (let i = 0; i < data.length; i++) {
-    const id = data[i].id;
-    const artworkLikes = await getLikes(id);
-    likes[id] = artworkLikes;
-  }
-
-  // Display artwork cards with likes
   data.forEach((element, index) => {
-    const id = element.id;
     Cards.innerHTML += `
       <div class="card">
         <div class="img-container">
@@ -25,15 +14,19 @@ async function DisplayCards(data) {
           </div>
         </div>
         <div class="title-container">
-          <h3>${element.title} <i class="fa fa-heart"></i></h3>
+          <h3>${element.title}</h3>
           <div class="interactions">
-            <div class="likes">${likes[id] || 0} likes</div>
+          <i class="fa fa-heart like-btn"></i>
+            <div class="likes likes-counter">0 Likes</div>
           </div>
           <button class="button comment"><i class="fa fa-comments"></i> Comments</button>
         </div>
       </div>
     `;
   });
+
+  const artworkCount = document.querySelector(".product-counter");
+  artworkCount.innerHTML = `(${data.length})`;
 
   const commentButtons = document.querySelectorAll(".comment");
   commentButtons.forEach((button, index) => {
@@ -43,24 +36,6 @@ async function DisplayCards(data) {
       openModal(modal, imgSrc, title, index);
     });
   });
-}
-
-const BASE_URL =
-  "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps";
-const APP_ID = "rZVQvLjlhB3dnDtkMDhH";
-
-async function getLikes(id) {
-  try {
-    const response = await fetch(`${BASE_URL}/${APP_ID}/likes`);
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    const data = await response.json();
-    return data.likes;
-  } catch (error) {
-    console.error("Error fetching likes:", error);
-    return 0; // Return 0 likes if an error occurs
-  }
 }
 
 export default DisplayCards;
