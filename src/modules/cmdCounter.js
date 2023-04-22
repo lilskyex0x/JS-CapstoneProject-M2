e67af1d814f466a577f8208681ca011547487197
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2023-04-22 18:36:20 +0630

    Fix test error inside the cmd counter

--- src/modules/cmdCounter.js
+++ src/modules/cmdCounter.js
@@ -1,5 +1,7 @@
+import { modal } from "./popup";
+
 const commentsCounter = (comments) => {
-  document.getElementById("c-total").textContent = comments.length;
+  modal.querySelector("#c-total").textContent = comments.length;
 };
 
 export default commentsCounter;
